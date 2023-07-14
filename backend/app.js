ec969dbf2181188e639087403d7d004c83d55a55
pandalite <arushi.singhal.cer21@itbhu.ac.in>
2023-07-15 00:09:17 +0530

    change

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 
 
-mongoose.connect('mongodb://localhost:27017/quotes-db')
+mongoose.connect('mongodb://127.0.0.1:27017/quotes-db')
     .then(() => console.log('Connection Open!'))
     .catch((err) => console.log(err));
 
